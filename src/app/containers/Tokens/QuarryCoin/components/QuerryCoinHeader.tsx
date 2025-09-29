cd47812d074ba9f529726897d49bde4720b0981e
jupiter229 <jupiterit29@gmail.com>
2025-09-29 11:03:04 -0700

    feat: update quarry ecosystem sub page

--- src/app/containers/Tokens/QuarryCoin/components/QuerryCoinHeader.tsx
+++ src/app/containers/Tokens/QuarryCoin/components/QuerryCoinHeader.tsx
@@ -13,25 +13,20 @@ const QuerryCoinHeader = () => {
 
       <div className="w-full flex flex-col md:flex-col lg:flex-row lg:justify-between items-center p-4">
         <div className="flex-1 lg:mr-4 mb-4 lg:mb-0">
-          <h2 className="text-[#553499] text-[40px]">Quarry Coin</h2>
+          <h2 className="text-[#553499] text-[40px]">Quarry (QRY)</h2>
         </div>
 
         <div className="w-40 h-40 flex-shrink-0">
           <img
             className="w-full h-full object-contain"
             src={assetPaths.QUARRY_IMAGE}
-            alt="Quary Coin"
+            alt="Quarry"
           />
         </div>
       </div>
       <div className="text-left mt-10">
         <li className="list-disc text-[#5B5B5B] text-[22px] mb-10 leading-[30px]">
-          Quarry Coin is the official cryptocurrency and utility token for the
-          QuarryChain Network. Quarry aims to be a peer-to-peer digital currency
-          that will enable lightning fast low cost transactions to anyone in the
-          world. Quarry will also be used to access and connect the entire
-          QuarryChain Ecosystem, with an abundance of application scenarios that
-          power transactions and applications on the chain.
+          Quarry (QRY) is the official cryptocurrency and utility token for the QuarryChain Network. Quarry aims to be a peer-to-peer digital currency that will enable lightning fast low cost transactions to anyone in the world. Quarry will also be used to access and connect the entire QuarryChain Ecosystem, with an abundance of application scenarios that power transactions and applications on the chain.
         </li>
       </div>
     </div>
